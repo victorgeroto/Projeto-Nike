4fd54180eab705c6354d273a254d9aaf80c484e6
victorgeroto <132359789+victorgeroto@users.noreply.github.com>
2024-12-03 15:29:01 -0300

    Update products.service.spec.ts

--- src/app/model/services/products.service.spec.ts
+++ src/app/model/services/products.service.spec.ts
@@ -1,16 +1,23 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ProductsService } from './products.service';
 
-describe('ProductsService', () => {
-  let service: ProductsService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ProductsService);
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppComponent],
+    }).compileComponents();
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
   });
+
+  it(`should have the 'sapatoCorrigido' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('sapatoCorrigido');
+  });
+
 });
